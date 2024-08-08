de781258c7efba31686e7a01b97e2032c10289e1
gtaitasaur <gtaitasaur@gmail.com>
2024-08-08 19:32:30 +0000

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/* INDEX.HTML */
+/* TASK ONE */
+const bntBorder = document.getElementById('tsk-one-btn-border');
+const imgFoxy = document.getElementById('tsk-1-img-foxy');
+bntBorder.addEventListener('click', () => {
+    if (imgFoxy.style.border) {
+      imgFoxy.style.border = ''; 
+    } else {
+      imgFoxy.style.border = '2px solid red'; 
+    }
+  });
+
+/* TASK TWO */
+
+/* TASK THREE */
